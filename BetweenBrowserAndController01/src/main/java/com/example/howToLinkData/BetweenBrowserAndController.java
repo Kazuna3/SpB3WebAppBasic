3e7418308d20b7fa8a9a2ff5764eb8820ee7f73c
K.M <kazunari@next.odn.ne.jp>
2024-04-01 17:16:31 +0900

    フォーム部品を増やして、それらの処理も追加した。

--- BetweenBrowserAndController01/src/main/java/com/example/howToLinkData/BetweenBrowserAndController.java
+++ BetweenBrowserAndController01/src/main/java/com/example/howToLinkData/BetweenBrowserAndController.java
@@ -1,6 +1,7 @@
 package com.example.howToLinkData;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,44 @@ public class BetweenBrowserAndController {
 
 	// http://localhost:8080
 	@GetMapping("/")
-	public String defaultHandler(
-		// @ModelAttribute InputOutputForm inputOutputForm
+	public String defaultHandler1(
 		@ModelAttribute InputOutputForm inOutF
 	) {
 
 		inOutF.setTextBox("初期値");
+
+		// パスワードには、何も値を設定できないみたい・・・
+		inOutF.setPassWord("初期値");
+
+		// html の radio の checked 属性が、無視される為、
+		// ラジオボタンの初期選択を設定する。
+		inOutF.setRadioBtn1("1");
+
+		// チェックボックスの初期選択を設定する。
+		// String[] strArray = { "1" };
+		// inOutF.setCheckbox2(strArray);
+		// 次行は、上２行と同じ処理である。
+		inOutF.setCheckbox2(new String[] { "1" });
+
+		return "elementPartsOfAForm";
+
+	}
+
+	// 
+	/**
+	 * defaultHandler1 メソッドの書き換え版。
+	 * 本メソッドの起動用 URL => http://localhost:8080
+	 * @param model
+	 * @return String ビューネームの文字列
+	 */
+	// @GetMapping("/")
+	public String defaultHandler2(
+		Model model
+	) {
+
+		InputOutputForm inOutF = new InputOutputForm();
+		inOutF.setTextBox("初期値");
+		model.addAttribute("inputOutputForm", inOutF);
 
 		return "elementPartsOfAForm";
 
@@ -31,7 +64,9 @@ public class BetweenBrowserAndController {
 
 		System.out.println(inOutF);
 
-		inOutF.setTextBox("変えちゃう");
+		// inOutF.setTextBox("変えちゃう");
+		// パスワードには、何も値を設定できないみたい・・・
+		// inOutF.setPassWord("変えちゃう");
 
 		return "elementPartsOfAForm";
 
--- BetweenBrowserAndController01/src/main/java/com/example/howToLinkData/form/InputOutputForm.java
+++ BetweenBrowserAndController01/src/main/java/com/example/howToLinkData/form/InputOutputForm.java
@@ -6,5 +6,24 @@ import lombok.Data;
 public class InputOutputForm {
 
 	private String textBox;
+	private String passWord;
+	private String radioBtn1;
+	private String radioBtn2;
+	private String checkbox1;
+
+	// ２つ以上のチェックボックスを、グループ化して使用する場合は、
+	// ［checkbox2］のように、そのデータ型は、String の配列が最善と考える。
+	// 数値型にすると、サーバー内部の変換処理で、実行時エラーが発生する恐れがあるが、
+	// String では、その心配が不要となる。
+	// ブラウザと Web Server 間のデータの連携は、文字データとして、やり取りしている。
+	// String の配列とする理由は、［elementPartsOfAForm.html］で
+	// ［チェックボックス２］と［チェックボックス３］の動作を比べると理解できると思う。
+	// 例えば、選択肢1,2,3を選ぶと、String の場合、連携されるデータは、［1,2,3］と言う文字列になる。
+	// それを、画面側に戻すことになるが、Thymelef の選択肢の value に［1,2,3］が無いため、
+	// 選択していた状態が、保てなくなるからである。
+	// String の配列を使用した場合は、正しく連携できる。
+	private String[] checkbox2;
+
+	private String checkbox3;
 
 }
