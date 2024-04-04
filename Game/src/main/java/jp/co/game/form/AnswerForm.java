6e5493280b59ad62378c6d663834d7c342a73852
K.M <kazunari@next.odn.ne.jp>
2024-04-04 17:11:09 +0900

    単項目チェックでエラーを検出した際のメッセージを修正した。

--- Game/src/main/java/jp/co/game/form/AnswerForm.java
+++ Game/src/main/java/jp/co/game/form/AnswerForm.java
@@ -9,7 +9,8 @@ public class AnswerForm {
 	// @formatter:off
 	@Pattern(
 		regexp = "\\d{1,3}?",
-		message = "単項目チェックでエラーを検出しました。１桁の数字以外のデータはエラーになります。"
+		message = "単項目チェックでエラーを検出しました。"
+		        + "１～３桁の数字以外のデータはエラーになります。"
 	)
 	// @formatter:on 
 	private String answerNumber;
