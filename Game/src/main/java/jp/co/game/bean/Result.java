adfb3432a26d5ee1b27cac3f443d0096d236153d
K.M <kazunari@next.odn.ne.jp>
2024-04-05 17:57:50 +0900

    ゲームの回答履歴データを保持するためのビーンを追加した。

---
+++ Game/src/main/java/jp/co/game/bean/Result.java
@@ -0,0 +1,21 @@
+package jp.co.game.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+
+	// 結果の番号を保持する。
+	private String seqNum;
+
+	// ゲームのプレーヤの答えた数値を保持する。
+	private String answerNumber;
+
+	// 数当てゲームの答え合わせの評価結果文字列を保持する。
+	private String result;
+
+}
