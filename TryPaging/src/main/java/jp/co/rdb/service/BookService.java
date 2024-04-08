4b0df8305e059259b0c106b3ad0f0402b94c5ebf
K.M <kazunari@next.odn.ne.jp>
2024-04-08 14:38:09 +0900

    ページネーションの使い方を研究するために作成したプロジェクト。

---
+++ TryPaging/src/main/java/jp/co/rdb/service/BookService.java
@@ -0,0 +1,24 @@
+package jp.co.rdb.service;
+
+import java.util.Optional;
+
+import jp.co.rdb.entity.Book;
+
+public interface BookService {
+
+	// Book テーブルのレコードを登録する
+	public void saveBook(Book book);
+
+	// クイズの全レコードを抽出する
+	public Iterable<Book> findAllBook();
+
+	// クイズのレコード件数を返す
+	public long countBook();
+
+	// クイズのレコードを削除する
+	public void deleteByIdBook(Integer id);
+
+	// id を検索条件として、クイズのレコードを検索し、その該当レコードを返す。
+	public Optional<Book> findByIdBook(Integer id);
+
+}
