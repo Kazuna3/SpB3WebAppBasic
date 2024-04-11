fcd3c746f5fe84c7fd64c15150d6fe96a0dcd303
K.M <kazunari@next.odn.ne.jp>
2024-04-11 13:55:55 +0900

    単一テーブルで、OR マッパーに、Spring Data JDBC を用いた、ページネーションの実装方法を研究するために作成したプロジェクト。

---
+++ TryPaginationSpringDataJDBC/src/main/java/jp/co/rdb/TryPaginationSpringDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package jp.co.rdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryPaginationSpringDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryPaginationSpringDataJdbcApplication.class, args);
+	}
+
+}
---
+++ TryPaginationSpringDataJDBC/src/main/java/jp/co/rdb/service/BookServiceImpl.java
@@ -0,0 +1,59 @@
+package jp.co.rdb.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.rdb.entity.Book;
+import jp.co.rdb.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+	private final BookRepository bookRepository;
+
+	// Book テーブルに、レコードを登録する。
+	@Override
+	public void saveBook(Book book) {
+
+		bookRepository.save(book);
+
+	}
+
+	// Book テーブルの全レコードを抽出する。
+	@Override
+	public Iterable<Book> findAllBook() {
+
+		Iterable<Book> iterableBook = bookRepository.findAll();
+		return iterableBook;
+
+	}
+
+	// Book テーブルのレコード件数を返す。
+	@Override
+	public long countBook() {
+
+		return bookRepository.count();
+
+	}
+
+	// Book テーブルのレコードを削除する。
+	@Override
+	public void deleteByIdBook(Integer id) {
+
+		bookRepository.deleteById(id);
+
+	}
+
+	// id を検索条件として、Book テーブルのレコードを検索し、その該当レコードを返す。
+	@Override
+	public Optional<Book> findByIdBook(Integer id) {
+
+		Optional<Book> book = bookRepository.findById(id);
+		return book;
+
+	}
+
+}
